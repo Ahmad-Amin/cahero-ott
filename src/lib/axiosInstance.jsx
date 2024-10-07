b0d2ad5410d1378d5441036e40612427a67f6140
Ahmad-Amin <baigahmad323@gmail.com>
2024-10-07 13:47:44 +0500

    changed the backend url

--- src/lib/axiosInstance.jsx
+++ src/lib/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "https://1da0-2407-d000-405-db1e-d458-e0f3-4f53-369a.ngrok-free.app/api",
+  baseURL: "https://7717-2407-d000-405-db1e-6bcf-de02-9b2e-3a73.ngrok-free.app/api",
 });
 
 axiosInstance.interceptors.request.use(
