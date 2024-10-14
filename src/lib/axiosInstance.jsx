469e5eeffbd2a518505f93114d9a506350741723
Ahmad-Amin <baigahmad323@gmail.com>
2024-10-14 23:34:50 +0500

    changed the api url

--- src/lib/axiosInstance.jsx
+++ src/lib/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: "https://cahero-ott-backend.onrender.com",
+  baseURL: "https://cahero-ott-backend.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use(
