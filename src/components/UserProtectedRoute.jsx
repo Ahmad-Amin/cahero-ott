9e376c241b0e5da10b336f49f60401e305fb0f0b
Ahmad-Amin <baigahmad323@gmail.com>
2024-11-17 02:31:18 -0800

    removed the unnecessary code

--- src/components/UserProtectedRoute.jsx
+++ src/components/UserProtectedRoute.jsx
@@ -1,20 +1,13 @@
-<<<<<<< Updated upstream
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { toast, isActive } from "react-toastify";
-=======
 // ProtectedRoute.js
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { toast } from "react-toastify";
->>>>>>> Stashed changes
 import { useSelector } from "react-redux";
 
 const UserProtectedRoute = ({ children }) => {
   const user = useSelector((state) => state.auth.user); 
 
   if (!user) {
-<<<<<<< Updated upstream
     if (!toast.isActive("login-required-toast")) { // Check if a toast with this ID is active
       toast.error("You need to log in to access this page.", {
         toastId: "login-required-toast", // Add a unique ID to the toast
@@ -28,9 +21,6 @@ const UserProtectedRoute = ({ children }) => {
         theme: "colored",
       });
     }
-=======
-    toast.error("Please login first!");
->>>>>>> Stashed changes
     return <Navigate to="/" />; 
   }
 
